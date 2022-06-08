d5916a2be097aa91bb511547f7de1d29e6e6f71a
xxduanxx :3 <josiasduan@gmail.com>
2022-06-08 04:24:47 +0000

    finalizacao do site

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash, request
+from flask import render_template, redirect, url_for, flash, request, abort
 from comunidadeimpressionadora import app, database, bcrypt
 from comunidadeimpressionadora.forms import FormCriarConta, FormLogin, FormEditarPerfil, FormCriarPost
 from comunidadeimpressionadora.models import Usuario, Post
@@ -108,3 +108,34 @@ def editar_perfil():
     foto_perfil = url_for('static', filename=f'fotos_perfil/{current_user.foto_perfil}')
 
     return render_template("editar_perfil.html",foto_perfil=foto_perfil, form=form,cursos=cursos)
+
+@app.route('/post/<post_id>', methods=['GET', 'POST'])
+def exibir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        form = FormCriarPost()
+        if request.method == 'GET':
+            form.titulo.data = post.titulo
+            form.corpo.data = post.corpo
+        elif form.validate_on_submit():
+            post.titulo = form.titulo.data
+            post.corpo = form.corpo.data
+            database.session.commit()
+            flash('Post atualizado','alert-success')
+            return redirect(url_for('homepage'))
+    else:
+        form=None
+
+    return render_template("post.html", post = post,form=form)
+    
+@app.route('/post/<post_id>/excluir', methods=['GET', 'POST'])
+@login_required
+def excluir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        database.session.delete(post)
+        database.session.commit()
+        flash('Post excluido com sucesso','alert-danger')
+        return redirect(url_for('homepage'))
+    else:
+        abort(403)
